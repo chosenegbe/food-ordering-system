48b3c337c7ab813b4b16ceb52d11a0799f9b0946
chosenegbe <milkymike45@gmail.com>
2022-07-25 10:28:58 +0200

    added missing property field batchSizeBoostFactor

--- infrastructure/kafka/kafka-config-data/src/main/java/com/food/ordering/system/kafka/config/data/KafkaProducerConfigData.java
+++ infrastructure/kafka/kafka-config-data/src/main/java/com/food/ordering/system/kafka/config/data/KafkaProducerConfigData.java
@@ -14,6 +14,7 @@ public class KafkaProducerConfigData {
     private String compressionType;
     private String acks;
     private Integer batchSize;
+    private Integer batchSizeBoostFactor;
     private Integer lingerMs;
     private Integer requestTimeoutMs;
     private Integer retryCount;
