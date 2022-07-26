2c5b02a62b86c4923b8d3901003b751dd667ddb0
chosenegbe <milkymike45@gmail.com>
2022-07-26 23:48:04 +0200

    added kafka producer service interface

--- infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/service/impl/KafkaProducerImpl.java
+++ infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/service/impl/KafkaProducerImpl.java
@@ -1,2 +1,16 @@
-package com.food.ordering.system.kafka.producer.service.impl;public class KafkaProducerImpl {
+package com.food.ordering.system.kafka.producer.service.impl;
+
+import com.food.ordering.system.kafka.producer.service.KafkaProducer;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.io.Serializable;
+
+public class KafkaProducerImpl<K extends Serializable, V extends SpecificRecordBase> implements KafkaProducer<K,V> {
+
+    @Override
+    public void send(String topicName, K key, V message, ListenableFutureCallback<SendResult<K, V>> callback) {
+
+    }
 }
