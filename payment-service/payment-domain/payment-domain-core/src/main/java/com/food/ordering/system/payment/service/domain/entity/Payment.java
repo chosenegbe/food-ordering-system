3027148967bd9ead72a32023b4fe674dce5a1b37
chosenegbe <milkymike45@gmail.com>
2022-08-10 20:19:13 +0200

    created payment-service module with submodules

---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com/food/ordering/system/payment/service/domain/entity/Payment.java
@@ -0,0 +1,6 @@
+package com.food.ordering.system.payment.service.domain.entity;
+
+import com.food.ordering.system.payment.service.domain.valueobject.PaymentId;
+
+public class Payment extends AggregateRoot<PaymentId>{
+}
