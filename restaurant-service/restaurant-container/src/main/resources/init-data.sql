50150091114d603502634acf8fbad2517a7d00d2
chosenegbe <milkymike45@gmail.com>
2022-08-14 00:06:02 +0200

    restaurant service - restaurant container

---
+++ restaurant-service/restaurant-container/src/main/resources/init-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO customer.customers(id, username, first_name, last_name)
+    VALUES('d215b5f8-0249-4dc5-89a3-51fd148cfb41', 'user-1', 'First', 'User');
+
+INSERT INTO customer.customers(id, username, first_name, last_name)
+    VALUES('d215b5f8-0249-4dc5-89a3-51fd148cfb42', 'user-2', 'Second', 'User');
