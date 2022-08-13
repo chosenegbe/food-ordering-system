874afb9c51118a60cebfb1fa3fdd06f3a29c9044
chosenegbe <milkymike45@gmail.com>
2022-08-13 22:08:48 +0200

    Restaurant data access implementation

--- restaurant-service/restaurant-data-access/src/main/java/com/food/ordering/system/restaurant/service/dataaccess/restauarant/repository/OrderApprovalJpaRepository.java
+++ restaurant-service/restaurant-data-access/src/main/java/com/food/ordering/system/restaurant/service/dataaccess/restauarant/repository/OrderApprovalJpaRepository.java
@@ -1,2 +1,11 @@
-package com.food.ordering.system.restaurant.service.dataaccess.restauarant.repository;public interface OrderApprovalJpaRepository {
+package com.food.ordering.system.restaurant.service.dataaccess.restauarant.repository;
+
+import com.food.ordering.system.restaurant.service.dataaccess.restauarant.entity.OrderApprovalEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface OrderApprovalJpaRepository extends JpaRepository<OrderApprovalEntity, UUID> {
 }
