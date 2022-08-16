8a919a0bffec4b7136d2361a95a2d18c7dc0a553
chosenegbe <milkymike45@gmail.com>
2022-08-16 23:36:41 +0200

    renaming of variables

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/valueobject/OrderItemId.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/valueobject/OrderItemId.java
@@ -2,7 +2,7 @@ package com.food.ordering.system.order.service.domain.valueobject;
 
 import com.food.ordering.system.valueobject.BaseId;
 
-public class OrderItemId  extends BaseId<Long> {
+public class OrderItemId extends BaseId<Long> {
     public OrderItemId(Long value) {
         super(value);
     }
