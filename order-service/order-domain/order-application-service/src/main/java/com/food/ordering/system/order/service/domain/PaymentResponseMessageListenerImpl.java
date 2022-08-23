9c8cf156608681cf90639a57fed252e01d5adf7e
chosenegbe <milkymike45@gmail.com>
2022-08-23 15:05:11 +0200

    PaymentResponseMessageListenerImpl

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/PaymentResponseMessageListenerImpl.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/PaymentResponseMessageListenerImpl.java
@@ -1,6 +1,8 @@
 package com.food.ordering.system.order.service.domain;
 
 import com.food.ordering.system.order.service.domain.dto.message.PaymentResponse;
+import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
+import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
 import com.food.ordering.system.order.service.domain.ports.input.message.listener.payment.PaymentResponseMessageListener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,13 +13,24 @@ import org.springframework.validation.annotation.Validated;
 @Validated
 @Service
 public class PaymentResponseMessageListenerImpl implements PaymentResponseMessageListener {
+
+    private final OrderPaymentSaga orderPaymentSaga;
+
+    public PaymentResponseMessageListenerImpl(OrderPaymentSaga orderPaymentSaga) {
+        this.orderPaymentSaga = orderPaymentSaga;
+    }
+
     @Override
     public void paymentCompleted(PaymentResponse paymentResponse) {
-
+        OrderPaidEvent orderPaidEvent = orderPaymentSaga.process(paymentResponse);
+        log.info("Publishing OrderPaidEvent for order id: {}", paymentResponse.getOrderId());
+        orderPaidEvent.fire();
     }
 
     @Override
     public void paymentCancelled(PaymentResponse paymentResponse) {
-
+        orderPaymentSaga.rollback(paymentResponse);
+        log.info("Order with id: {} is roll back with failure messages: {}", paymentResponse.getOrderId()
+            , String.join(",", paymentResponse.getFailureMessages()));
     }
 }
