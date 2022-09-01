006509b75cd0e763e28e4271dd065ec5485b98ab
chosenegbe <milkymike45@gmail.com>
2022-09-01 17:58:57 +0200

    clean up projects, formatting and removing unnecessary imports + bug fix

--- restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordering/system/restaurant/service/domain/RestaurantDomainServiceImpl.java
+++ restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordering/system/restaurant/service/domain/RestaurantDomainServiceImpl.java
@@ -1,6 +1,5 @@
 package com.food.ordering.system.restaurant.service.domain;
 
-import com.food.ordering.system.event.publisher.DomainEventPublisher;
 import com.food.ordering.system.restaurant.service.domain.entity.Restaurant;
 import com.food.ordering.system.restaurant.service.domain.event.OrderApprovalEvent;
 import com.food.ordering.system.restaurant.service.domain.event.OrderApprovedEvent;
